8c16da0708838ae6adbce08ee35b0ef7b6f61876
DESKTOP-639MRKN\WinHuma <koolala001@gmail.com>
2018-01-17 23:00:50 +0700

    เพิ่มเพจสองกับเพจสาม และ รวมโค้ดหน้าแรก

---
+++ firstPage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+
+
+</head>
+<body >
+  <div id = "showDDD">
+  <h1>Page 1</h1>
+  <p>Page content 3</p>
+  <ol>
+    <li><a href="decay.html">backkkkkk</a></li>
+  </ol>
+</div>
+
+
+
+
+</body>
+</html>
