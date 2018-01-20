bd55485baa2d646b3987c161ce9b74f256393260
DESKTOP-639MRKN\WinHuma <koolala001@gmail.com>
2018-01-20 22:08:07 +0700

    เปลี่ยนขนาดแผนที่เล็กน้อย เปลี่ยนรูปแบบเมนูด้านข้าง(เป็นปุ่ม) ทำการเชื่อมโยงแต่ละหน้าเป็นปุ่มแทน(เพื่อความสวยงาม) หน้า report สามารถไปกลับได้ทั้ง marker และ search

--- Report.php
+++ Report.php
@@ -1,22 +1,48 @@
-<!DOCTYPE html>
-<html>
-<head>
+<div id = "showDDD">
+<h1>Report</h1>
 
+<table style="width:800px">
+  <tr>
+    <td>พื้นที่นี้เพาะปลูกข้าวได้</td>
+    <td><button class="btn-success" onclick="toggle_visibility('foo1');">รายละเอียด</button></td>
+  </tr>
+</table>
+<div class="bg-info" id="foo1" style="display:none;">
 
+  เพราะอย่างนู้น อย่างนี้ อย่างนั้น</br>
+  เพราะนั้น นู้น นี้ 11111111
 
-
-</head>
-<body >
-  <div id = "showDDD">
-  <h1>Page 1</h1>
-  <p>Page content 3</p>
-  <ol>
-    <li><a href="decay.html">backkkkkk</a></li>
-  </ol>
 </div>
 
+  <table style="width:800px">
+  <tr>
+    <td>พื้นที่นี้ เหมาะสำหรับการปลูกข้าวรูปแบบ นาขั้นบันได้</td>
+    <td><button class="btn-success" onclick="toggle_visibility('foo2');">รายละเอียด</button></td>
+  </tr>
+</table>
+  <div class="bg-info" id="foo2" style="display:none;">
+
+    เพราะอย่างนู้น อย่างนี้ อย่างนั้น</br>
+    เพราะนั้น นู้น นี้ 2222222222
+
+  </div>
 
 
+  <table style="width:800px">
+  <tr>
+    <td>พันธุ์ข้าวที่เหมาะสม คือ กข15 </td>
+    <td><button class="btn-success" onclick="toggle_visibility('foo3');">รายละเอียด</button></td>
+  </tr>
+</table>
 
-</body>
-</html>
+<div class="bg-info" id="foo3" style="display:none;">
+
+  เพราะอย่างนู้น อย่างนี้ อย่างนั้น</br>
+  เพราะนั้น นู้น นี้ 333333333
+
+</div>
+
+    <a href="search.html">กลับไปหน้าค้นหา</a></br>
+    <button class="btn btn-info" onclick='location.replace("marker.html")'>กลับไปยังหน้าเมนูหลัก</button>
+
+</div>
